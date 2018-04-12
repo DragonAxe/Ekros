9f333d4b0d0d04f6981ce5c192c529cc5ad56b91
DragonAxe <DragonAxe@users.noreply.github.com>
2018-04-12 13:22:21 -0600

    Fixed flying camera controls

--- Glitter/Sources/Camera.cpp
+++ Glitter/Sources/Camera.cpp
@@ -23,26 +23,14 @@ public:
             pressed = false;
         }
 
-        if (key == GLFW_KEY_W) {
-            forwardPressed = pressed;
-        }
-        if (key == GLFW_KEY_S) {
-            backwardPressed = pressed;
-        }
+        forwardPressed = (key == GLFW_KEY_W) ? pressed : forwardPressed;
+        backwardPressed = (key == GLFW_KEY_S) ? pressed : backwardPressed;
 
-        if (key == GLFW_KEY_A) {
-            leftPressed = pressed;
-        }
-        if (key == GLFW_KEY_D) {
-            rightPressed = pressed;
-        }
+        leftPressed = (key == GLFW_KEY_A) ? pressed : leftPressed;
+        rightPressed = (key == GLFW_KEY_D) ? pressed : rightPressed;
 
-        if (key == GLFW_KEY_SPACE) {
-            upPressed = pressed;
-        }
-        if (key == GLFW_KEY_LEFT_SHIFT) {
-            downPressed = pressed;
-        }
+        upPressed = (key == GLFW_KEY_SPACE) ? pressed : upPressed;
+        downPressed = (key == GLFW_KEY_LEFT_SHIFT) ? pressed : downPressed;
     }
 
     void mouseCallback(GLFWwindow* window __attribute__((unused)),
@@ -52,47 +40,48 @@ public:
         cursorYPosition = ypos;
     }
 
+    /**
+     * Should be called every frame.
+     */
     void update()
     {
+        // Calculate orientation of camera
         glm::mat4 rmx = glm::rotate(glm::mat4(1.0f),
-                                    (float) (cursorXPosition / 100.0f),
+                                    (float) (cursorXPosition / 500.0f),
                                     glm::vec3(0, 1, 0));
         glm::mat4 rmy = glm::rotate(glm::mat4(1.0f),
-                                    (float) (cursorYPosition / 100.0f),
+                                    (float) (cursorYPosition / 500.0f),
                                     glm::vec3(1, 0, 0));
         orientation = rmy * rmx;
 
+        // Calculate orientation of movement vector
+        glm::mat4 reflectmx = glm::mat4(-1.0, 0.0, 0.0, 0.0,
+                                        0.0, 1.0, 0.0, 0.0,
+                                        0.0, 0.0, 1.0, 0.0,
+                                        0.0, 0.0, 0.0, 1.0);
+        glm::mat4 reflectmy = glm::mat4(1.0, 0.0, 0.0, 0.0,
+                                        0.0, -1.0, 0.0, 0.0,
+                                        0.0, 0.0, 1.0, 0.0,
+                                        0.0, 0.0, 0.0, 1.0);
+        glm::mat4 reflectedOrientation = (reflectmx * rmx) * (reflectmy * rmy);
 
-        glm::vec3 movementVec = glm::vec3();
-        if (forwardPressed) {
-            movementVec.z += 0.01f;
-        }
-        if (backwardPressed) {
-            movementVec.z -= 0.01f;
-        }
-        if (leftPressed) {
-            movementVec.x += 0.01f;
-        }
-        if (rightPressed) {
-            movementVec.x -= 0.01f;
-        }
-        if (upPressed) {
-            movementVec.y -= 0.01f;
-        }
-        if (downPressed) {
-            movementVec.y += 0.01f;
-        }
-        glm::vec3 a = orientation * glm::vec4(movementVec, 1.0f);
+        // Calculate axis aligned movement vector from key presses
+        glm::vec3 moveVec = glm::vec3();
+        moveVec.z += (forwardPressed) ? 1.0f : (backwardPressed) ? -1.0f : 0;
+        moveVec.x += (rightPressed) ? 1.0f : (leftPressed) ? -1.0f : 0;
+        moveVec.y += (upPressed) ? 1.0f : (downPressed) ? -1.0f : 0;
+        moveVec *= 0.1f;
 
-        position += a;
+        // Move camera by oriented movement vector
+        position += (glm::vec3) // Why I have to cast I don't know. C++ is stupid
+                (reflectedOrientation * glm::vec4(moveVec, 1.0f));
     }
 
     glm::mat4 getCameraMatrix()
     {
+        glm::mat4 posM = glm::translate(glm::mat4(1.0f), position);
 
-        glm::mat4 vm = glm::translate(glm::mat4(1.0f), position);
-
-        glm::mat4 proj = glm::perspective(
+        glm::mat4 perspectiveM = glm::perspective(
                 glm::radians(90.0f), // Fov in degrees
                 // Aspect ratio
                 (float) DisplayManager::getWindowWidth() /
@@ -100,7 +89,7 @@ public:
                 0.01f,   // Near fov
                 1000.f); // Far fov
 
-        return proj * orientation * vm;
+        return perspectiveM * orientation * posM;
     }
 
 private:
@@ -116,6 +105,5 @@ private:
     bool downPressed = false;
     glm::vec3 position = glm::vec3();
     glm::mat4 orientation = glm::mat4();
-    glm::mat4 cameraMatrix;
 };
 
